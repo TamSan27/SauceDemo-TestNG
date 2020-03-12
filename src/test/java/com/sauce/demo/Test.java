59af286ddd15809776eb9c8ab77058e20848e1ac
TamSan27 <se.tamil333@gmail.com>
2020-03-12 07:41:45 +0530

    modified test.java

--- src/test/java/com/sauce/demo/Test.java
+++ src/test/java/com/sauce/demo/Test.java
@@ -1,5 +1,15 @@
 package com.sauce.demo;
 
-public class Test {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class Test {
+	public static void main(String[] args) throws InterruptedException  {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Tamil\\git\\SauceDemo-TestNG\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://toolsqa.com/handling-alerts-using-selenium-webdriver/");
+		
+}
 }
