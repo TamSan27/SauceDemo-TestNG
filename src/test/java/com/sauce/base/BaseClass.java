4abb8aa50b0fccfb55620b49ed0551dd59813efa
TamSan27 <se.tamil333@gmail.com>
2020-03-12 08:19:02 +0530

    modified base class code

--- src/test/java/com/sauce/base/BaseClass.java
+++ src/test/java/com/sauce/base/BaseClass.java
@@ -1,5 +1,24 @@
 package com.sauce.base;
 
-public class BaseClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class BaseClass {
+	
+         private static WebDriver driver;
+
+		public static WebDriver getDriver() {
+			return driver;
+		}
+
+		public static void main(String[] args) {
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\Tamil\\git\\SauceDemo-TestNG\\drivers\\chromedriver.exe");
+			 driver = new ChromeDriver();
+			getDriver().get("http://toolsqa.com/handling-alerts-using-selenium-webdriver/");
+			System.out.println("added");
+			
+			
+		}
+		
+         
 }
