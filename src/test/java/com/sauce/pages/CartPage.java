b33c357e4074cd539d8eb4e6f7dee53b940fb941
TamSan27 <se.tamil333@gmail.com>
2020-03-18 07:32:33 +0530

    common page added

--- src/test/java/com/sauce/pages/CartPage.java
+++ src/test/java/com/sauce/pages/CartPage.java
@@ -1,5 +1,13 @@
 package com.sauce.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CartPage {
-//added
+	@FindBy(xpath="//div[text()='Sauce Labs Backpack']")
+	private WebElement Sauce_Labs_Backpack ;
+
+	public WebElement getFirst_Product() {
+		return Sauce_Labs_Backpack;
+	}
 }
