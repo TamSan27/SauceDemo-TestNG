85eb6d23a46816d9ae47bc9e77fdf58ebee3c02f
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2020-03-18 07:54:37 +0530

    added common page xpaths

--- src/test/java/com/sauce/pages/HomePage.java
+++ src/test/java/com/sauce/pages/HomePage.java
@@ -10,10 +10,26 @@ import com.sauce.base.BaseClass;
 		//@FindBy(xpath = "//div//div[@class='inventory_list']//div[1]//div[3]//button[1]")
 		//@FindBy(xpath="//div[@class='inventory_item'][1]//button")
 		@FindBy(xpath="//div[text()='Sauce Labs Backpack']//parent::a//parent::div/following-sibling::div//button")
-		private WebElement First_Product ;
+		private WebElement AddProduct_Sauce_Labs_Backpack ;
+		
+		@FindBy(xpath="//div[text()='Sauce Labs Bike Light']//parent::a//parent::div/following-sibling::div//button")
+		private WebElement AddProduct_Sauce_Labs_Bike_Light ;
 
-		public WebElement getFirst_Product() {
-			return First_Product;
+		@FindBy(xpath="//div[text()='Sauce Labs Bolt T-Shirt']//parent::a//parent::div/following-sibling::div//button")
+		private WebElement AddProduct_Sauce_Labs_Bolt_TShirt ;
+		
+		@FindBy(xpath="//div[text()='Sauce Labs Fleece Jacket']//parent::a//parent::div/following-sibling::div//button")
+		private WebElement AddProduct_Sauce_Labs_Fleece_Jacket ;
+		
+		@FindBy(xpath="//div[text()='Test.allTheThings() T-Shirt (Red)']//parent::a//parent::div/following-sibling::div//button")
+		private WebElement AddProduct_Sauce_Labs_Red_Tshirt ;
+		
+		@FindBy(xpath="//div[text()='Sauce Labs Onesie']//parent::a//parent::div/following-sibling::div//button")
+		private WebElement AddProduct_Sauce_Labs_Onesie ;
+		
+		
+
+	
 		}
 		
 		
