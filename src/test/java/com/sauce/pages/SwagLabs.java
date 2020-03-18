157f5e975165e865cdc4b0af1c808b576c7be198
TamSan27 <se.tamil333@gmail.com>
2020-03-18 07:13:07 +0530

    changed xpath ion swaglabs

--- src/test/java/com/sauce/pages/SwagLabs.java
+++ src/test/java/com/sauce/pages/SwagLabs.java
@@ -8,7 +8,8 @@ import com.sauce.base.BaseClass;
 	public class SwagLabs extends BaseClass{
 		//@FindBy(xpath="//button[@class=\"btn_primary btn_inventory\"])
 		//@FindBy(xpath = "//div//div[@class='inventory_list']//div[1]//div[3]//button[1]")
-		@FindBy(xpath="//div[@class='inventory_item'][1]//button")
+		//@FindBy(xpath="//div[@class='inventory_item'][1]//button")
+		@FindBy(xpath="//div[text()='Sauce Labs Backpack']//parent::a//parent::div/following-sibling::div//button")
 		private WebElement First_Product ;
 
 		public WebElement getFirst_Product() {
